659a9ba542e6db09ac8778e61b785cc1c540e472
Wolnei Cordeiro <wolneicordeiro@gmail.com>
2025-09-04 18:34:02 -0300

    Menu Links + Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/theme.css'
 import './styles/global.css'
@@ -12,8 +12,9 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
 
     </>
