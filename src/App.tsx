56b072bf6cdf6b2a263a167545e9c9c4eb6b6320
Wolnei Cordeiro <wolneicordeiro@gmail.com>
2025-10-03 00:46:13 -0300

    Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import './styles/theme.css'
 import './styles/global.css'
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 
 
 export function App() {
@@ -41,7 +43,8 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon/>} color='green'/>
+            <DefaultButton icon={<StopCircleIcon/>} color='red'/>
           </div>
 
         </form>
