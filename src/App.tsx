236369891e20b0c59e43c5b3a368ed012e9767b7
Wolnei Cordeiro <wolneicordeiro@gmail.com>
2025-10-03 14:47:43 -0300

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 
 export function App() {
@@ -44,11 +45,14 @@ export function App() {
 
           <div className="formRow">
             <DefaultButton icon={<PlayCircleIcon/>} color='green'/>
-            <DefaultButton icon={<StopCircleIcon/>} color='red'/>
           </div>
 
         </form>
 
+      </Container>
+
+      <Container>
+        <Footer/>
       </Container>
 
     </>
